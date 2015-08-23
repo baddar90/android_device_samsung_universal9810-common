9c57ce9a9fde9754844241f81cfa8af6a8aa36c0
jrizzoli <joey@cyanogenmoditalia.it>
2015-08-23 17:41:41 +0200

    star: releasetools: Run resize2fs on /system partition
    Change-Id: I6877347ec2d4af20b1afe708241fe06403e293bd
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -4,3 +4,8 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system");');
+
+def FullOTA_PostValidate(info):
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
+    info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
+    info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/11120000.ufs/by-name/SYSTEM");');
