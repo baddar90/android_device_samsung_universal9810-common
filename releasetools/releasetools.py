70a00f570e296a1bc9cfc33fbf3ce0307e54841a
Jesse Chan <jc@lineageos.org>
2018-10-17 21:33:04 -0700

    universal9810: assert on vendor image version
    Change-Id: Ia74576e547bb3e45d13c06b6255f42d94b5e0e14

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,3 +1,21 @@
+def FullOTA_Assertions(info):
+    info.script.AppendExtra('ifelse(is_mounted("/vendor"), unmount("/vendor"));');
+    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/11120000.ufs/by-name/VENDOR", "/vendor");');
+    info.script.AppendExtra('ifelse(file_getprop("/vendor/build.prop","ro.vendor.build.date.utc") != "1536890700",');
+    info.script.AppendExtra('unmount("/vendor");');
+    info.script.AppendExtra('ui_print(" ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("                    ERROR:                   ");');
+    info.script.AppendExtra('ui_print("           Vendor image on the device        ");');
+    info.script.AppendExtra('ui_print("               is NOT compatible             ");');
+    info.script.AppendExtra('ui_print("           Expected version: 27_ARI9         ");');
+    info.script.AppendExtra('ui_print("        You can download from is.gd/omexok   ");');
+    info.script.AppendExtra('ui_print("=============================================");');
+    info.script.AppendExtra('ui_print("============NO CHANGE HAS BEEN MADE==========");');
+    info.script.AppendExtra('abort(" "););');
+    info.script.AppendExtra('unmount("/vendor");');
+
 def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/11120000.ufs/by-name/SYSTEM", "/system");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
