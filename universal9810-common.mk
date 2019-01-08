de2325769ff432c568a6b8ee1d0f3f41c302fa92
ivanmeler <i_ivan@windowslive.com>
2019-01-08 11:50:59 +0100

    universal9810: bluetooth: set permissions
    Change-Id: I10c282c6c4298774bb1f950b6361fd6a11e9fb9c

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -9,6 +9,8 @@ endif
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.heartrate.xml:system/etc/permissions/android.hardware.sensor.heartrate.xml \
