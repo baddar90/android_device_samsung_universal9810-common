28fba2fcbc32c98dc5cdd39ae2470041df1c07bb
padarshr <padarshr@codeaurora.org>
2019-04-25 17:58:17 +0530

    universal9810: Address 'LOCAL_MODULE_TAGS := eng debug'  deprecation.
    Newer AOSP deprecates LOCAL_MODULE_TAGS := eng debug.
    We now remove that and instead add the package to
    PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    To ensure that we don't miss out on any package during this switch,
    we add these explicitly, even though it might have been already
    added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := recovery.fstab
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/recovery.fstab
 LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := twrp.fstab
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/twrp.fstab
 LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
@@ -18,7 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.recovery.usb.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.recovery.usb.rc
 LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
