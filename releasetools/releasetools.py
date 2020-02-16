5682c00c4239049bb2732f7c9a33f0f6316ee017
Erfan Abdi <erfangplus@gmail.com>
2020-02-16 18:03:18 +0330

    universal9810: releasetools: Fix sed path on Q SAR
    Change-Id: Iaf81e5e7aef6dc080f35e22167e1b7e3a9c466d2

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,7 +1,7 @@
 def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/11120000.ufs/by-name/SYSTEM", "/system");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('unmount("/system");');
 
 def FullOTA_PostValidate(info):
