66c780d728c93a3c4d7cc3e51901bcba4710fa37
Erfan Abdi <erfangplus@gmail.com>
2020-02-17 15:06:43 +0330

    universal9810: Use vendor_overlay for light hal
    Change-Id: I23c19aa37edf2c5b16ccb6f05bfe423766293440

--- lights/Android.mk
+++ lights/Android.mk
@@ -18,26 +18,41 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    Light.cpp \
-    service.cpp
+LOCAL_MODULE := android.hardware.light@2.0-service.9810
+LOCAL_MODULE_TAGS  := optional
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := vendor.samsung.hardware.light@3.0-service
+
+LOCAL_SRC_FILES := \
+    service.cpp \
+    Light.cpp
+
+LOCAL_REQUIRED_MODULES := \
+    android.hardware.light@2.0-service.9810.rc
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
-    liblog \
-    libdl \
-    libbinder \
+    libhardware \
     libhidlbase \
     libhidltransport \
-    libhardware \
+    liblog \
+    libhwbinder \
     libutils \
     android.hardware.light@2.0
 
-LOCAL_MODULE := android.hardware.light@2.0-service.9810
-LOCAL_INIT_RC := android.hardware.light@2.0-service.9810.rc
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.9810.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := vendor.samsung.hardware.light@3.0-service.rc
+
+LOCAL_SRC_FILES := android.hardware.light@2.0-service.9810.rc
+
+include $(BUILD_PREBUILT)
