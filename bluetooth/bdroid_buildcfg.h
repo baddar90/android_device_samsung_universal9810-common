d57319f6b906da0727982efc7b0acd013f92e764
Erfan Abdi <erfangplus@gmail.com>
2020-02-18 12:33:30 +0330

    universal9810: Set bt default name based on device codename
    Change-Id: Ifc2e75117a026282ee011c7ef4d3bf63bf241f52

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,26 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy"
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+    
+    if (strstr(product_device, "starlte"))
+        return "Galaxy S9";
+    if (strstr(product_device, "star2lte"))
+        return "Galaxy S9+";
+    if (strstr(product_device, "crownlte"))
+        return "Galaxy Note 9";
+    
+    // Fallback to Default
+    return "Samsung Galaxy";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
