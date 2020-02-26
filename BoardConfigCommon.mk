6debffb727ea9b60d82f4232b2d3ef6ef8309534
Erfan Abdi <erfangplus@gmail.com>
2020-02-26 15:36:24 +0330

    universal9810: Nuke releasetools
    Change-Id: Ie79b3fb0747dab851f680b6dd48ae0b09ac6feeb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,9 +129,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
-
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
