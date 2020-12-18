7318e495254a08e7d6915b9bd51e7a28ff4fa928
yarpiin <yarpiin@gmail.com>
2020-12-18 08:19:48 +0000

    universal9810: add push to wake

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -102,7 +102,8 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl
+    $(LOCAL_PATH)/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_touchscreen.kl
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
