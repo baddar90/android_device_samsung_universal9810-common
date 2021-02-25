a64293086c11707bdf8501d20aa29773307e8d99
baddar90 <mnbaddar1990@gmail.com>
2021-02-25 23:45:48 +0400

    disable adb props

--- system_prop.mk
+++ system_prop.mk
@@ -37,9 +37,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.cmc.version=2.0
 
 # ADB on boot
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    persist.sys.usb.config=mtp,adb \
-    ro.debuggable=1 \
-    ro.adb.secure=0
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    persist.service.adb.enable=1 \
+#    persist.service.debuggable=1 \
+#    persist.sys.usb.config=mtp,adb \
+#    ro.debuggable=1 \
+#    ro.adb.secure=0
