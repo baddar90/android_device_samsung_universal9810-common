04df13eda7d79cbce1f411f56c363809ae133a83
TH779 <i@779.moe>
2021-10-12 20:53:24 +0400

    Skip ELF prebuilt copy check

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 COMMON_PATH := device/samsung/universal9810-common
 
